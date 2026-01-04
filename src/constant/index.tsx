b8162f1fc6dff1d9a9e1ccfb975e8f19df9d298c
fiezalhag <fiezalhag@gmail.com>
2026-01-04 11:56:05 +0300

    add DURATION_LABELS and DURATIONS

--- src/constant/index.tsx
+++ src/constant/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 export const AvatarGroupData = [
   {
     id: 1,
@@ -37,3 +38,33 @@ export const messagesEn: string[] = [
   "Trusted by over 10,000 patients  now it’s your turn",
   "Sharper diagnostics. Faster answers. Clearer results",
 ];
+
+export const DURATION_LABELS = {
+  ar: {
+    "أقل من يوم": "أقل من يوم",
+    "1-3 أيام": "1-3 أيام",
+    "أسبوع": "أسبوع",
+    "أسبوعين": "أسبوعين",
+    "شهر": "شهر",
+    "أكثر من شهر": "أكثر من شهر",
+  },
+  en: {
+    "أقل من يوم": "Less than a day",
+    "1-3 أيام": "1–3 days",
+    "أسبوع": "One week",
+    "أسبوعين": "Two weeks",
+    "شهر": "One month",
+    "أكثر من شهر": "More than a month",
+  },
+} as const;
+
+
+export const DURATIONS = [
+  "أقل من يوم",
+  "1-3 أيام",
+  "أسبوع",
+  "أسبوعين",
+  "شهر",
+  "أكثر من شهر",
+] as const;
+
