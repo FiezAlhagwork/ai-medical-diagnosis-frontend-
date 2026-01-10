80a3bacf31ad2bb3ee9ebac1da0270637e3e8c63
fiezalhag <fiezalhag@gmail.com>
2026-01-10 16:00:47 +0300

    install Axios to the project and configure AxiosInstance

---
+++ src/utils/axiosinstance.ts
@@ -0,0 +1,28 @@
+import axios from "axios"
+
+// const url = import.meta.env.VITE_BASE_Url
+
+const axiosInstance = axios.create({
+    baseURL:"http://localhost:5000",
+    timeout:10000,
+    headers:{
+        "Content-Type":"application/json"
+    }
+})
+
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const accessToken = localStorage.getItem("token")
+        if(accessToken){
+            config.headers.Authorization = `Bearer ${accessToken}`
+        }
+        return config
+    },
+
+    (error) => {
+        return Promise.reject(error)
+    }
+)
+
+export default axiosInstance
