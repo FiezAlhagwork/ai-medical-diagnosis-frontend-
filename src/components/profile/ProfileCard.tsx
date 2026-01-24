bf890de5013a3b3b2bbc69db0674a2eb95c72aa9
fiezalhag <fiezalhag@gmail.com>
2026-01-24 18:39:48 +0300

    We have modified the translation files.

--- src/components/profile/ProfileCard.tsx
+++ src/components/profile/ProfileCard.tsx
@@ -2,13 +2,15 @@ import { Link } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 import Button from '../ui/Button'
 import { FaRegEdit } from "react-icons/fa";
+import { useTranslation } from 'react-i18next';
 
 
 const ProfileCard = () => {
     const { user } = useAuth()
+    const { t } = useTranslation("profile")
     return (
         <>
-            <h1 className="text-center text-3xl  md:text-5xl font-bold">ملفي الشخصي</h1>
+            <h1 className="text-center text-3xl  md:text-5xl font-bold">{t("title")}</h1>
             <div className="bg-white flex flex-col md:flex-row items-center text-center  gap-4 mt-20 py-10 shadow-lg rounded-3xl">
                 <div className="w-20 h-20 rounded-full bg-linear-to-r from-cyan-500 to-blue-500 text-white flex items-center justify-center text-3xl font-bold md:ms-9">
                     F
@@ -24,7 +26,7 @@ const ProfileCard = () => {
                 <div className="md:pe-7">
                     <Link to="/symptoms" >
                         <Button icon={<FaRegEdit size={22} />} variant="primary">
-                            انشاء تشخيص
+                            {t("create_diagnosis")}
                         </Button>
                     </Link>
                 </div>
